a3dfb2f21d3ad56c1e0f1685122c45e85ab49f91
vodcaster <121347401+vodcaster@users.noreply.github.com>
2023-09-19 20:23:47 +0000

    arrow_functions

--- 01Basics/this_basics.js
+++ 01Basics/this_basics.js
@@ -19,3 +19,18 @@ const arrowFunc = () => {
     // console.log(this)
 }
 arrowFunc()
+
+
+//+++++++++++++++++++++++++++++++++more about arrow functions++++++++++++++++++++++++++++++++++++++
+
+// implicit and explicit return
+
+const sum = (num1,num2) => {
+    return num1+num2   //explicit return
+}
+
+const add = (num1,num2) => num1+num2 //implicit return
+const addTwo = (num1,num2) => (num1+num2 ) //implicit return
+// console.log(sum(2,4))
+// console.log(add(4,6))
+// console.log(addTwo(6,8))
